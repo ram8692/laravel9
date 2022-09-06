4091ad00453b520d1b9ad17d2aac1a0decb94ac4
rambabu.g <rambabu.g@whatarage.com>
2022-09-06 20:00:03 +0530

    initial commit with controller and route

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('hello');
+// });
+
+// Route::get('/', function () {
+//     return redirect('about');
+// });
+
+//  Route::get('/{name}', function ($name) {
+//      return view('about',['names'=>$name]);
+//  });
+
+
+ Route::get("user/{id}",[UserController::class,"show"]);
+Route::view("about",'/about');
