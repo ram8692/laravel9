42f1269b891971d71dd0431a03172188f81f63d6
rambabu.g <rambabu.g@whatarage.com>
2022-09-27 19:11:48 +0530

    DeferrableProvider

--- app/Providers/PaymentServiceProvider.php
+++ app/Providers/PaymentServiceProvider.php
@@ -3,9 +3,12 @@
 namespace App\Providers;
 
 use App\Service\PaypalService;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-class PaymentServiceProvider extends ServiceProvider
+//DeferrableProvider is used when provider when needed in that case we can use DeferrableProvider
+
+class PaymentServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     /**
      * Register services.
@@ -17,6 +20,12 @@ class PaymentServiceProvider extends ServiceProvider
         $this->app->bind(PaypalService::class, function(){
             return new PaypalService("thetestcoder-".rand(1,1500));
         });
+    }
+
+    //below function is provided by DeferrableProvider
+    public function provides()
+    {
+        return [PaypalService::class];
     }
 
     /**
